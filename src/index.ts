ff0f48b2d890f8179cb1822574b377e03f36b52d
JesusGattorossi <93558045+JesusGattorossi@users.noreply.github.com>
2022-01-12 16:04:19 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,100 @@
+//calcula el area de un rectangulo
+//let base:number = 2;
+//let altura:number = 6;
+//let area = (base * altura);
+//console.log(" el resultado es "+ area);
+
+// ingrese el usuario 2 numeros que se resten y muestren el resultado
+//let numeroa : number = 3;
+//let numerob : number = 6;
+//let resultado = ( numeroa - numerob);
+//console.log("el resultado es " + resultado);
+
+//
+//let base : number = 2;
+//let altura: number = 6;
+//let area:number;
+//area= base * altura;
+//console.log("el area es" + area);
+
+//  calcular tiempo total de 4 vueltas y tiempo promedio de vueltas
+
+//let vuelta1: number = Number(1.18);
+//let vuelta2: number = Number(1.15);
+//let vuelta3: number = Number(1.20);
+//let vuelta4: number = Number(1.12);
+//let tiempoTotal = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+//console.log("el tiempo total es", +tiempoTotal);
+///console.log( "el tiempo promedio de vuelta es, +"tiempoTotal/4);
+
+//precio inicial 450.50, descuento a aplicar 10%
+//let PrecioInicial: number =(450);
+//let descuento: number= (0.1);
+//let PrecioFinal: number= (PrecioInicial - descuento);
+//console.log("el precio final es", +450 * 0.1);
+
+//para subir a una montaña rusa tenes que medir 1.65 o mas
+//let NumeroMontañaRusa: number = 1.65;
+//let NumeroDeseado: number = 1.90;
+//if (NumeroDeseado > NumeroMontañaRusa) {
+//console.log("el numero es mayor a 1.65", + NumeroDeseado);
+//} else {
+//console.log("el numero es menor o igual a 1.65", + NumeroDeseado);
+//}
+
+//let numero1: number=2;
+//let numero2: number=3;
+//resultado : Number = numero1< numero2
+//console.log( "el resultado es ",+ numero1 <numero2)
+//let lucesEncendidas : boolean = false;
+//let litrosNafta : number = 10;
+//if (lucesEncendidas || litrosNafta > 0) {
+// console.log("Puedo manejar de noche");
+//}else{
+//console.log("no puedo manejar de noche");
+//}
+
+
+//ejercicio
+//let monto: number = 6500;
+//let cantidad: number = 2;
+//let montoConDescuento: number = 0;
+//let descuento: number = 0;
+//let precioTotal: number = 0;
+//precioTotal = monto * cantidad;
+//if (precioTotal >= 1000) {
+//descuento = (precioTotal * 10) / 100;
+//montoConDescuento = precioTotal - descuento;
+//console.log("por gastar mas de 1000 tiene un 10% de descuento");
+//console.log("el precio a pagar es", +montoConDescuento);
+//} else {
+//console.log("no tiene descuento. El monto a pagar es", +precioTotal);
+//}
+
+
+//para subir a un juego debe medir mas de 1.30
+//let numPermitido: number = 1.30;
+//let numPersona: number = 1.90;
+//if (numPermitido <= numPersona) {
+//console.log("sube al juego", +numPermitido <= numPersona);
+//} else {
+//console.log("No sube al juego", +numPermitido >=numPersona);
+//}
+
+
+// medallas
+//let posicionLlegada: number= 1;
+//if (posicionLlegada==1){
+  //console.log("medalla de oro", + posicionLlegada);
+//} else{
+//if(posicionLlegada==2){
+//console.log("medalla de plata",+ posicionLlegada==2);
+//}else{
+//if(posicionLlegada==3){
+//console.log("medalla de bronce", + posicionLlegada==3);
+//}else{
+//if(posicionLlegada>=4){
+//console.log("certificado de participacion",+ posicionLlegada>=4);
+// }
+//}
+//}
